a54b938591ce419d039475bf579143912f9d1f2b
Lala <luangu2020@gmail.com>
2020-07-25 19:20:44 -0400

    added htmlroutes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 //routes
 require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true
