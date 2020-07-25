080cf9dfe977408da8e618ac13a41bc6c883366a
Lala <luangu2020@gmail.com>
2020-07-25 18:15:29 -0400

    creating the server and the routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+router.post("api/workout", (req, res) => {
+  Workout.create({}).then()
+});
+
+module.exports = app => {
+
+  //getLastWorkout (api.js)
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(dbWorkouts => {
+        res.json(dbWorkouts);
+      })
+      .catch(err => {
+        res.json(err)
+      });
+  });
+
+  //addExercise (api.js)
+  app.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exercises: req.body } }
+    )
+      .then(dbWorkouts => {
+        res.json(dbWorkouts);
+      })
+      .catch(er => {
+        res.json(err)
+      });
+  });
+
+  //createWorkout (api.js)
+  app.post("/api/workouts", (req, res) => {
+    Workout.create({})
+      .then(dbWorkouts => res.json(dbWorkouts))
+      .catch(err => {
+        res.json(err)
+      })
+  });
+
+  //getWorkoutsInRange (api.js)
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find()
+      .then(dbWorkouts => {
+        res.json(dbWorkouts)
+      })
+      .catch(err => {
+        res.json(err)
+      })
+  });
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
+
+app.listen(PORT, function () {
+  console.log(`Now listening on port: ${PORT}`);
+});
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+mongoose.connect(MONGODB_URI)
